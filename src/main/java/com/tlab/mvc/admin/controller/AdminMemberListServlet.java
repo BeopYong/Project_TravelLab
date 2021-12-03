b79cf31563693bde6eeb014c929b6eace29e707e
jimmerland <iloveinca2@gmail.com>
2021-12-03 23:09:00 +0900

    magazine scrap servlet and jsp small fix

--- src/main/java/com/tlab/mvc/admin/controller/AdminMemberListServlet.java
+++ src/main/java/com/tlab/mvc/admin/controller/AdminMemberListServlet.java
@@ -23,29 +23,6 @@ public class AdminMemberListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private MemberService memberService = new MemberService();
 	
-	/**
-	 * sql 
-	 * select * from member order by enroll_date desc
-	 * 
-	 * paging recipe
-	 * 1. content section
-	 *  - cPage 현재페이지
-	 *  - numPerPage 한페이지의 게시물수 10
-	 * 	- startNum, endNum
-	 * 
-	 * 
-	 * 2. pagebar section
-	 * 	- totalContent 총게시물수
-	 *  - totalPage 12
-	 *  - pagebarSize 5
-	 *  - pageNo 증감변수
-	 *  - pageStart | pageEnd
-	 *  - url
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1.사용자입력값
 		try {
@@ -63,14 +40,14 @@ public class AdminMemberListServlet extends HttpServlet {
 			// 2.업무로직
 			// 2-a. content영역
 			List<Member> list = memberService.selectAllMember(param);
-			System.out.println("list@servlet = " + list);
+			System.out.println("list@MemberListServlet = " + list);
 			// 2-b. pagebar영역
 			int totalContent = memberService.selectTotalMemberCount();
 			String url = request.getRequestURI(); // /mvc/admin/memberList
 			System.out.println(totalContent);
 			System.out.println(url);
 			String pagebar = MvcUtils.getPagebar(cPage, numPerPage, totalContent, url);
-			System.out.println("pagebar@servlet = " + pagebar);
+			System.out.println("pagebar@MemberListServlet = " + pagebar);
 			
 			// 3.view단처리
 			request.setAttribute("list", list);
